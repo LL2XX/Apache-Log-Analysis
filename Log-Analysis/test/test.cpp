32a25a1e383d7d7994400ad9f878b5e479bc93f4
X_spider_X <w1516067@outlook.com>
2023-03-14 17:07:02 +0800

    Add files via upload

---
+++ Log-Analysis/test/test.cpp
@@ -0,0 +1,9 @@
+﻿#include "../libLogAnalysis/include/Log-Analysis.h"
+int main()
+{
+	Log_Analysis log("../data/access_log_20220603-155424.log");
+	log.time_analysis();
+	log.ip_analysis();
+	log.user_analysis();
+	return 0;
+}
